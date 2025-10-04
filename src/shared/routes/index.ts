851eeb05cc6ebc5a70ed22f9d5b2ed76f353d8b0
Ricky <thechaosmyth7@gmail.com>
2025-10-04 23:13:19 +0700

    created landing page

--- src/shared/routes/index.ts
+++ src/shared/routes/index.ts
@@ -3,6 +3,12 @@ import type { RouteRecordRaw } from "vue-router"
 import { UtilsPageName } from "../models"
 
 const utilRoutes: RouteRecordRaw[] = [
+  // delete or change this later
+  {
+    path: "/",
+    name: "LandingPage",
+    component: () => import("@/shared/views/LandingPageView.vue"),
+  },
   {
     path: "/:pathMatch(.*)*",
     name: UtilsPageName.PAGE_NOT_FOUND,
